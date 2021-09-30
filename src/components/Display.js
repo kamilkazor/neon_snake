6a622c2cb2cf271192b2bf68458c3ba8cc648fe6
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-09-30 23:16:41 +0200

    Added field and fieldStyle

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,14 +1,23 @@
 const Display = () => {
-  const displaySize = "500px"
-
+  const displaySize = "500px";
+  const fieldSize = "10px"
+  
   const displayStyle = {
     width: displaySize,
     height: displaySize,
     border: "1px solid black"
   }
+  const fieldStyle = {
+    width: fieldSize,
+    height: fieldSize,
+    backgroundColor: "black",
+    border: "1px solid white"
+  }
   
   return (
-    <div style={displayStyle}></div>
+    <div style={displayStyle}>
+      <div style={fieldStyle}></div>
+    </div>
   );
 }
 
