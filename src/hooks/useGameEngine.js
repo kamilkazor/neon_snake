a953f32dcd73a2bd7ab16e6c70121ebe8f9b7212
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-01 18:56:45 +0200

    Fixed delay in useGameEngine

--- src/hooks/useGameEngine.js
+++ src/hooks/useGameEngine.js
@@ -1,17 +1,13 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 const useGameEngine = (gameSpeed, gameUpdate) => {
   const [running, setRunning] = useState(false);
   const [loopNum, setLoopNum] = useState(0);
 
-  const update = () => {
-    gameUpdate()
-    setLoopNum(loopNum + 1)
-  }
-
   useEffect(() => {
     if(running){
-      setTimeout(update, gameSpeed)
+      gameUpdate()
+      setTimeout(() => {setLoopNum(loopNum + 1)}, gameSpeed)
     }
   },[running, loopNum])
 
