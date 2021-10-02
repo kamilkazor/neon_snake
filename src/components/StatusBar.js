e3aede5983b00c68c0fce83e05c3c9522c631909
kamilkazor <79405091+kamilkazor@users.noreply.github.com>
2021-10-02 18:30:45 +0200

    Added bigger text to message top

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -2,7 +2,7 @@ const StatusBar = ({message, snakeLength}) => {
   return (
     <div className="statusBar neonText">
       <div className="messageBox">
-        <div>{message.top}</div>
+        <div className="biggerText">{message.top}</div>
         <div>{message.bottom}</div>
       </div>
       <div>SNAKE LENGTH: {snakeLength}</div>
